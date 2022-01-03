6e7961a48ebc2b43f18dbca5faee0580748ea70a
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2022-01-04 00:11:37 +0800

    clear some unused code and annotate

--- src/My_Qt_Player.cpp
+++ src/My_Qt_Player.cpp
@@ -10,14 +10,8 @@ My_Qt_Player::My_Qt_Player(QWidget *parent)
 	std::wstring::size_type pos = std::wstring(buffer).find_last_of((L"\\/"));
 	exec_path = std::wstring(buffer).substr(0, pos);
 
-	//ui.volume_slider->setRange(1, 99);
-	//ui.volume_slider->setValue(1);
 	QTableWidgetItem* newitem = new QTableWidgetItem(QObject::tr("name"));
 	ui.audio_list_table_widget->setHorizontalHeaderItem(0, newitem);
-	//connect(ui.volume_slider, &QSlider::valueChanged, this, &My_Qt_Player::set_volume);
-
-	//file_name.resize(100);
-	//file_name.constFind();
 
 	connect(ui.volume_slider, &QSlider::valueChanged, this, [&](const int volume) {
 			if (is_player)
@@ -43,7 +37,7 @@ std::wstring My_Qt_Player::GetVideoFilePath()
 	CoCreateInstance(CLSID_FileOpenDialog, NULL, CLSCTX_ALL,
 		IID_IFileOpenDialog, reinterpret_cast<void**>(FileDialog.GetAddressOf()));
 
-	FileDialog->SetTitle(L"¿ï¾ÜÀÉ®×");
+	FileDialog->SetTitle(L"é¸æ“‡æª”æ¡ˆ");
 	COMDLG_FILTERSPEC rgSpecial[] =
 	{
 		{L"VIDEO", L"*.*"},
@@ -73,16 +67,6 @@ void My_Qt_Player::on_OpenFile_button_clicked() //debug function
 
 void My_Qt_Player::on_Play_Button_clicked()
 {
-	//get table item
-	//auto it = file_name.constFind(item...)
-	//if (it != file_name.constEnd())
-	/*
-		file_path = QString().fromStdString(w2s(exec_path + "temp" + std::to_wstring(it.second)))
-		media = ...QByteArray(file_path.toLocal8Bit()).constData()
-	else
-		QMessageBox()
-		return;
-	*/
 	auto it = file_name.constFind(ui.audio_list_table_widget->currentItem()->text());
 	if (it != file_name.constEnd() && !is_player)
 	{
@@ -114,7 +98,6 @@ void My_Qt_Player::on_Play_Button_clicked()
 			libvlc_media_list_player_stop(media_list_player);
 			libvlc_media_list_player_release(media_list_player);
 			file_path = QString::fromStdString(w2s(exec_path + L"\\temp" + std::to_wstring(it.value()) + L".webm"));
-			//instance = libvlc_new(0, NULL);
 			media_list_player = libvlc_media_list_player_new(instance);
 			auto media = libvlc_media_new_path(instance, QByteArray(file_path.toLocal8Bit()).constData());
 			media_player = libvlc_media_player_new_from_media(media);
@@ -162,13 +145,11 @@ void My_Qt_Player::on_Pause_Button_clicked()
 {
 	if (playerstate == STATE_PLAY)
 	{
-		//libvlc_media_player_set_pause(media_player, 1);
 		libvlc_media_list_player_pause(media_list_player);
 		playerstate = STATE_PAUSE;
 	}
 	else if (playerstate == STATE_PAUSE)
 	{
-		//libvlc_media_player_set_pause(media_player, 0);
 		libvlc_media_list_player_play(media_list_player);
 		playerstate = STATE_PLAY;
 	}
@@ -186,33 +167,8 @@ void My_Qt_Player::on_Stop_Button_clicked()
 void My_Qt_Player::on_Add_new_audio_button_clicked()
 {
 	if (ui.url_line_edit->text().isEmpty() || !ui.url_line_edit->text().contains("http")) return;
-	//TODO : compete insert new table download file 
-
-	//on_Add_debug_button_clicked(const QString& url)
-	//QString title_text{};
-	//{
-	//	QString title_file_path = qApp->applicationDirPath() + "/title.txt";
-	//	qDebug() << title_file_path << "\n";
-	//	QFile title(title_file_path);
-	//	if (!title.open(QIODevice::ReadWrite | QIODevice::Text))
-	//	{
-	//		qDebug() << title.errorString() << " " << title.error() << "\n";
-	//		QMessageBox::information(nullptr, "debug", "file can't open");
-	//	}
-	//	else
-	//	{
-	//		QByteArray title_array{ title.readAll() };
-	//		title_text = QString(title_array);
-	//		//title.resize(0);
-	//		title.close();
-	//	}
-	//	qDebug() << title_text << '\n';
-	//}
-	// call yt-dlp to get audio title
-	// command += L"\\yt-dlp_x86.exe --get-title "; command += (std::wstring)url; command += L" >> title.txt";
-	// call_yt_dlp(command)
-	// QString title = get_title();
-	// 
+	//TODO : get title
+	
 	ui.audio_list_table_widget->insertRow(ui.audio_list_table_widget->rowCount());
 	//set [rowCount() - 1][0] = title;
 	//set [rowCount() - 1][1] = processing
@@ -239,15 +195,6 @@ void My_Qt_Player::on_Add_new_audio_button_clicked()
 	call_yt_dlp(temp);
 	processing_item = new QTableWidgetItem("process finished");
 	ui.audio_list_table_widget->setItem(ui.audio_list_table_widget->rowCount() - 1, 1, processing_item);
-
-	/*for (const auto& x : file_name)
-		qDebug() << x << " ";
-	qDebug() << "\n";*/
-	//cnt++;
-	//set [rowCount()][1] = "³B²z¤¤"
-	//call yt-dlp to get audio file
-	//file name = temp+QString::number(rowCount())
-	//if yt-dlp is closed -> set [rowCount()][1] = "³B²z§¹²¦"
 	
 }
 
@@ -307,6 +254,30 @@ void My_Qt_Player::call_yt_dlp(const std::wstring& command)
 
 QString My_Qt_Player::get_title()
 {
+	//QString title_text{};
+	//{
+	//	QString title_file_path = qApp->applicationDirPath() + "/title.txt";
+	//	qDebug() << title_file_path << "\n";
+	//	QFile title(title_file_path);
+	//	if (!title.open(QIODevice::ReadWrite | QIODevice::Text))
+	//	{
+	//		qDebug() << title.errorString() << " " << title.error() << "\n";
+	//		QMessageBox::information(nullptr, "debug", "file can't open");
+	//	}
+	//	else
+	//	{
+	//		QByteArray title_array{ title.readAll() };
+	//		title_text = QString(title_array);
+	//		//title.resize(0);
+	//		title.close();
+	//	}
+	//	qDebug() << title_text << '\n';
+	//}
+	// call yt-dlp to get audio title
+	// command += L"\\yt-dlp_x86.exe --get-title "; command += (std::wstring)url; command += L" >> title.txt";
+	// call_yt_dlp(command)
+	// QString title = get_title();
+	// 
 	return QString();
 }
 
